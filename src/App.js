dfb15159c53b1d6c8526894e321031e675bc4a1e
Igor Tuag <igortuag@gmail.com>
2020-05-20 07:02:51 -0300

    Limpando a implementação para usar o Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,20 +9,17 @@ import Sorteio from './components/Sorteio';
 
 function App() {
 
-  const [min, setMin] = useState(10)
-  const [max, setMax] = useState(20)
-
   return (
     <div className="App">
       <h1>Execício React-Redux</h1>
       <div className="linha">
-        <Intervalo min={min} max={max} onMinChanged={setMin} onMaxChanged={setMax}/>
+        <Intervalo />
 
       </div>
       <div className="linha">
-        <Media min={min} max={max}/>
-        <Soma min={min} max={max}/>
-        <Sorteio min={min} max={max}/>
+        <Media />
+        <Soma />
+        <Sorteio />
       </div>
     </div>
   );
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
-    const { min, max } = props
 
     return (
         <Card title="Sorteio de um Números" purple>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>{parseInt(Math.random() * (max - min) + min)}</strong>
+                    <strong>{10}</strong>
                 </span>
             </div>
         </Card>
